3a2e893601813e84de971c27356d758a7297f07d
Hongjin Li <vic.hong@rock-chips.com>
2025-05-28 10:14:58 +0800

    python: update pkg usage

--- 2.python/1.Base/1.syntax/6.packages/demo_pkg/__init__.py
+++ 2.python/1.Base/1.syntax/6.packages/demo_pkg/__init__.py
@@ -5,5 +5,10 @@ Demo Package for Python
 This is a simple demo package for Python with basic functionalities.
 """
 
+# 外部以 package 的形式进行 import 时，只能访问到这里import过的内容
+# 外部如果以 module 的形式进行 import 时，这里不受影响
+
+# 相对导入
 from .math_operations import add, subtract
-from .string_operations import to_upper, to_lower
+# 绝对导入
+from demo_pkg.string_operations import to_upper, to_lower
--- 2.python/1.Base/1.syntax/6.packages/tests/test_demo_pkg.py
+++ 2.python/1.Base/1.syntax/6.packages/tests/test_demo_pkg.py
@@ -1,20 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+
+root_dir = Path(__file__).parent.parent
+sys.path.append(str(root_dir))
 
 import unittest
-from demo_pkg import add, subtract, to_upper, to_lower
+import demo_pkg as mpkg
+# 或者如下导入，调用的时候就不需要加mpkg前缀
+# from demo_pkg import *
 
 class TestDemoPkg(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(mpkg.add(2, 3), 5)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(mpkg.subtract(10, 5), 5)
 
     def test_to_upper(self):
-        self.assertEqual(to_upper("hello"), "HELLO")
+        self.assertEqual(mpkg.to_upper("hello"), "HELLO")
 
     def test_to_lower(self):
-        self.assertEqual(to_lower("WORLD"), "world")
+        self.assertEqual(mpkg.to_lower("WORLD"), "world")
 
 if __name__ == '__main__':
     unittest.main()
